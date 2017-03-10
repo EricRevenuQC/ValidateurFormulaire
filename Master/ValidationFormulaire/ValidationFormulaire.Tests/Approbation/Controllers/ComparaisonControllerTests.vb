d208622bd3a091c58df425fa23307ca25dd1fa96
Zoheir <zoheir.ghazi@revenuquebec.ca>
2017-03-10 13:27:32 -0500

    Comparison tests start

--- Master/ValidationFormulaire/ValidationFormulaire.Tests/Approbation/Controllers/ComparaisonControllerTests.vb
+++ Master/ValidationFormulaire/ValidationFormulaire.Tests/Approbation/Controllers/ComparaisonControllerTests.vb
@@ -7,21 +7,30 @@ Imports ValidationFormulaire.Models
 Imports ValidationFormulaire.Core
 Imports ValidationFormulaire.Core.DeterminePage
 Imports System.Drawing
+Imports System.IO
+Imports System.Drawing.Imaging
 
 <TestClass()> _
 Public Class ComparaisonControllerTests
     Private comparaison_controller As ComparisonController
     Private session_value_provider As Mock(Of SessionValueProvider)
     Private pagination As Mock(Of IPagination)
-    Private converter As Mock(Of IConverter)
+    'Private converter As Mock(Of IConverter)
     Private config As Mock(Of IConfig)
+    Private file_left As HttpPostedFileBase
+    Private file_right As HttpPostedFileBase
+    Private file_stream As FileStream
+    Private converter As PDFToImageConverter
 
+    Private file_path = Path.GetFullPath("../../Files/formulaire.pdf")
 
     <TestInitialize()> _
     Public Sub Initialize()
         session_value_provider = New Mock(Of SessionValueProvider)
         pagination = New Mock(Of IPagination)
-        converter = New Mock(Of IConverter)
+        'converter = New Mock(Of IConverter)
+        converter = New PDFToImageConverter()
+      
 
         config = New Mock(Of IConfig)
         session_value_provider.Setup(Function(f) f.GetValue("current_page_left")).Returns(2)
@@ -29,7 +38,7 @@ Public Class ComparaisonControllerTests
 
     <TestInitialize()> _
     Private Sub InitController()
-        comparaison_controller = New ComparisonController(pagination:=pagination.Object, converter:=converter.Object,
+        comparaison_controller = New ComparisonController(pagination:=pagination.Object,
                                                              session_value_provider:=session_value_provider.Object,
                                                              config:=config.Object)
 
@@ -38,12 +47,19 @@ Public Class ComparaisonControllerTests
 
     <TestMethod()> _
     Public Sub ComparaisonFormulaireSetSessionForCurrentPageToCorrectPageForNextPageAction()
-        pagination.Setup(Function(f) f.DeterminePageChange(PageAction.NextPage, 2, Nothing)).Returns(3)
+        pagination.Setup(Function(f) f.DeterminePageChange(PageAction.NextPage, 2, Nothing)).Returns(2)
         InitController()
         comparaison_controller.ComparisonFormulaire(PageAction.NextPage)
-        session_value_provider.Verify(Sub(f) f.SetValue("current_page_left", 3), Times.Exactly(1))
-
+        session_value_provider.Verify(Sub(f) f.SetValue("comparing", "none"))
+        session_value_provider.Verify(Sub(f) f.SetValue("non_comparing", "block"))
     End Sub
+
+    
+
+
+
+
+
 
 
 
---
+++ Master/ValidationFormulaire/ValidationFormulaire.Tests/Approbation/Controllers/CompraisonTests.vb
@@ -0,0 +1,42 @@
+﻿Imports System.IO
+Imports ValidationFormulaire.Models
+Imports System.Drawing
+Imports ValidationFormulaire.Core
+Imports System.Web
+Imports Moq
+Imports System.Drawing.Imaging
+
+<TestClass()> _
+Public Class CompraisonTests
+    Private comparaison_controleur As ComparisonController
+    Private file_left_test As Mock(Of HttpPostedFileBase)
+    Private session_value_provider As SessionValueProvider
+    Private converter As PDFToImageConverter
+    Private file_stream As FileStream
+    Private file_Path = Path.GetFullPath("../../Files/formulaire111.pdf")
+
+
+
+    <TestInitialize()> _
+    Public Sub Initialize()
+        comparaison_controleur = New ComparisonController()
+        converter = New PDFToImageConverter()
+        file_Path = Path.GetFullPath("../../Files/formulaire111.pdf")
+        file_stream = New FileStream(file_Path, FileMode.Open)
+    End Sub
+
+    <TestMethod()> _
+    Public Sub ComparisonControllerFileLeftConversionPDFToImage()
+        Dim images() As Image
+        file_left_test = New Mock(Of HttpPostedFileBase)
+        file_left_test.Setup(Function(f) f.InputStream).Returns(file_stream)
+
+        images = converter.PDFToImage(file_left_test.Object)
+        images = comparaison_controleur.ComparisonFormulaire
+        For i As Integer = 1 To images.Length - 1
+            Assert.IsTrue(images(i).RawFormat.Equals(ImageFormat.MemoryBmp))
+        Next
+
+
+    End Sub
+End Class
