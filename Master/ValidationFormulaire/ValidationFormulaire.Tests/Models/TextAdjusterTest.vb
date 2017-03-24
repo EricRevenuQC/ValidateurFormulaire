9790b203439ab4993db73f581dc95baecf42a24a
Ghazi Zoheir <RGHZ001@PROD.MRQ>
2017-03-24 11:55:36 -0400

    test added

---
+++ Master/ValidationFormulaire/ValidationFormulaire.Tests/Models/TextAdjusterTest.vb
@@ -0,0 +1,56 @@
+﻿Imports System.Web
+Imports System.Text
+Imports ValidationFormulaire.Core
+Imports System.IO
+
+
+<TestClass()> _
+Public Class TextAdjusterTest
+    Private remove_char As TextAdjuster
+    Private chain, text, result As String
+    Private number_position As Integer
+    Private characters() As Char
+    Private chars_to_remove As TextAdjuster
+
+    <TestInitialize()> _
+    Public Sub initialize()
+
+        remove_char = New TextAdjuster()
+        chars_to_remove = New TextAdjuster()
+    End Sub
+
+    <TestMethod()> _
+    Public Sub RemoveCharsChain()
+        Dim chain As String
+        Dim text As String
+        Dim result As String
+        Dim number_position As Integer
+
+
+        number_position = 0
+        text = "abcdefgh"
+        chain = "abcd"
+
+        result = remove_char.RemoveCharChainFromText(chain, text(4))
+
+        Assert.AreEqual("e", result)
+    End Sub
+
+    <TestMethod()> _
+    Public Sub RemovCharFromNumbers()
+        Dim characters() As Char = " "
+        Dim result As String
+
+
+
+
+        result = remove_char.RemoveCharsFromNumbersInString(characters:=",", text:="1235abcd2,efj")
+
+        Assert.AreEqual("1235abcd2,efj", result)
+
+        result = remove_char.RemoveCharsFromNumbersInString(characters:=",", text:="1235abcd2,3efj")
+
+        Assert.AreEqual("1235abcd23efj", result)
+
+    End Sub
+End Class
---
+++ Master/ValidationFormulaire/ValidationFormulaire.Tests/Models/TextExtractorTest.vb
@@ -0,0 +1,43 @@
+﻿Imports iTextSharp
+Imports System.Web
+Imports System.Text
+Imports ValidationFormulaire.Core
+Imports System.IO
+Imports Moq
+
+
+<TestClass()> _
+Public Class TextExtractorTest
+    Private text_extractor As TextExtractor
+    Private file_path As String
+    Private file_stream As FileStream
+    Private file_mock As Mock(Of HttpPostedFileBase)
+
+    <TestInitialize()> _
+    Public Sub initialize()
+        text_extractor = New TextExtractor()
+        file_path = Path.GetFullPath("../../Files/pdftest1.pdf")
+        file_stream = New FileStream(file_path, FileMode.Open)
+    End Sub
+
+    <TestCleanup()> _
+    Public Sub CleanUp()
+        file_stream.Close()
+    End Sub
+    <TestMethod()> _
+    Public Sub ExtractTextFromPDFReturnText()
+        file_mock = New Mock(Of HttpPostedFileBase)
+        file_mock.Setup(Function(f) f.InputStream).Returns(file_stream)
+        Dim text_result() As String
+
+        text_result = text_extractor.PDFToText(file_mock.Object)
+
+        For i As Integer = 1 To text_result.Length - 1
+            Assert.AreEqual("pdftestx ", text_result(i))
+        Next
+
+
+    End Sub
+
+End Class
+
