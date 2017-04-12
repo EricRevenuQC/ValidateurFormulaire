90f389b53f3e089df5df218c6079a8fc5a0825e0
Simard Éric (DGTT) <RSIE016@PROD.MRQ>
2017-04-12 10:08:05 -0400

    fix start

---
+++ Master/ValidationFormulaire/ValidationFormulaire.Core/Interfaces/PDFText/Combination/ICombineText.vb
@@ -0,0 +1,3 @@
+﻿Public Interface ICombineText
+    Function CombineText(text_groups As Object) As Object
+End Interface
--- Master/ValidationFormulaire/ValidationFormulaire.Core/Model/PDFText/Combination/CombineCloseText.vb
+++ Master/ValidationFormulaire/ValidationFormulaire.Core/Model/PDFText/Combination/CombineCloseText.vb
@@ -1,12 +1,12 @@
 ﻿Imports System.Drawing
 
 Public Class CombineCloseText
-    Implements ICombinePDFText
+    Implements ICombineText
 
     Private Const MAX_DISTANCE_BETWEEN_CHARACTERS_FOR_X As Integer = 10
     Private Const MAX_DISTANCE_BETWEEN_CHARACTERS_FOR_Y As Integer = 5
 
-    Public Function CombineText(text_groups As Object) As Object Implements ICombinePDFText.CombineText
+    Public Function CombineText(text_groups As Object) As Object Implements ICombineText.CombineText
         Dim word As String = ""
         Dim current_x_left As Integer = 0
         Dim current_x_right As Integer = 0
